52ea88a70810a98a276bf78986ffab7a21c4c978
kausar_uddin <kausarbd22@gmailcom>
2020-01-02 20:40:46 +0600

    Dropdown

---
+++ app/src/main/java/com/myapp/dropdown/SignUpActivity.java
@@ -0,0 +1,35 @@
+package com.myapp.dropdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Spinner mySpinner=findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> myAdapter= ArrayAdapter.createFromResource(this,
+                 R.array.Division,android.R.layout.simple_spinner_item);
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+        mySpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
